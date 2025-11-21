d6e9acaa8029e7607a2e5e5962269d4d0d0d3581
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2025-11-22 00:25:25 +0600

    Updated download page test
    * Added more test
    * Added more locators

--- test_google_files_app/tests/test_download_page.py
+++ test_google_files_app/tests/test_download_page.py
@@ -13,3 +13,19 @@ class TestDownloadPage():
         driver = driver
         self.download_page = DownloadPage(driver)
         assert self.download_page.verify_header_elements()
+
+    def test_header_root_button_functionality(self, driver):
+        driver = driver
+        self.download_page = DownloadPage(driver)
+        assert self.download_page.check_header_root_button_functionality()
+
+    def test_header_search_functionality(self, driver):
+        driver = driver
+        self.download_page = DownloadPage(driver)
+        assert self.download_page.check_header_search_accessibility(search_text="no match")
+        assert self.download_page.message_text == "No matches in Downloads"
+
+    def test_more_options_accessibility(self, driver):
+        driver = driver
+        self.download_page = DownloadPage(driver)
+        assert self.download_page.check_more_options_accessibility()
